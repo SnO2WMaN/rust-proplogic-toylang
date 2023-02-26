6904e042216e1be7a99b58d926d7cdbf2e6c25b5
SnO2WMaN <me@sno2wman.net>
2023-02-26 11:24:06 +0900

    indent 2

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,15 @@ mod tokenizer;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
-    eval: String,
+  #[arg(short, long)]
+  eval: String,
 }
 
 fn main() -> Result<()> {
-    let args = Args::parse();
+  let args = Args::parse();
 
-    let tokens = tokenizer::tokenize(&args.eval)?;
-    println!("Tokens: {:?}", tokens);
+  let tokens = tokenizer::tokenize(&args.eval)?;
+  println!("Tokens: {:?}", tokens);
 
-    Ok(())
+  Ok(())
 }
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -2,122 +2,122 @@ use anyhow::Result;
 
 #[derive(Debug)]
 enum LogicFn0 {
-    TRUE,
-    FALSE,
+  TRUE,
+  FALSE,
 }
 
 #[derive(Debug)]
 enum LogicFn1 {
-    NOT,
+  NOT,
 }
 
 #[derive(Debug)]
 enum LogicFn2 {
-    AND,
-    OR,
-    IMP,
-    EQ,
+  AND,
+  OR,
+  IMP,
+  EQ,
 }
 
 #[derive(Debug)]
 enum Symbol {
-    Variable { name: char },
-    LogicFn0(LogicFn0),
-    LogicFn1(LogicFn1),
-    LogicFn2(LogicFn2),
-    ParenL,
-    ParenR,
-    Comma,
+  Variable { name: char },
+  LogicFn0(LogicFn0),
+  LogicFn1(LogicFn1),
+  LogicFn2(LogicFn2),
+  ParenL,
+  ParenR,
+  Comma,
 }
 
 #[derive(Debug)]
 pub struct Token {
-    symbol: Symbol,
-    start: i64,
-    end: i64,
+  symbol: Symbol,
+  start: i64,
+  end: i64,
 }
 
 pub fn tokenize(input: &str) -> Result<Vec<Token>> {
-    let mut tokens: Vec<Token> = Vec::new();
-    tokens.push(Token {
-        symbol: Symbol::LogicFn2(LogicFn2::AND),
-        start: 0,
-        end: 1,
-    });
-    tokens.push(Token {
-        symbol: Symbol::ParenL,
-        start: 1,
-        end: 2,
-    });
-    tokens.push(Token {
-        symbol: Symbol::Variable { name: 'p' },
-        start: 2,
-        end: 3,
-    });
-    tokens.push(Token {
-        symbol: Symbol::Comma,
-        start: 3,
-        end: 4,
-    });
-    tokens.push(Token {
-        symbol: Symbol::Variable { name: 'q' },
-        start: 4,
-        end: 5,
-    });
-    tokens.push(Token {
-        symbol: Symbol::ParenR,
-        start: 5,
-        end: 6,
-    });
+  let mut tokens: Vec<Token> = Vec::new();
+  tokens.push(Token {
+    symbol: Symbol::LogicFn2(LogicFn2::AND),
+    start: 0,
+    end: 1,
+  });
+  tokens.push(Token {
+    symbol: Symbol::ParenL,
+    start: 1,
+    end: 2,
+  });
+  tokens.push(Token {
+    symbol: Symbol::Variable { name: 'p' },
+    start: 2,
+    end: 3,
+  });
+  tokens.push(Token {
+    symbol: Symbol::Comma,
+    start: 3,
+    end: 4,
+  });
+  tokens.push(Token {
+    symbol: Symbol::Variable { name: 'q' },
+    start: 4,
+    end: 5,
+  });
+  tokens.push(Token {
+    symbol: Symbol::ParenR,
+    start: 5,
+    end: 6,
+  });
 
-    Ok(tokens)
+  Ok(tokens)
 }
 
 #[cfg(test)]
 mod test {
-    use crate::tokenizer::tokenize;
+  use crate::tokenizer::tokenize;
 
-    #[test]
-    fn var() {
-        tokenize("p");
-        tokenize("q");
-        tokenize("r");
-        tokenize("s");
-        tokenize("t");
-    }
+  #[test]
+  fn var() {
+    tokenize("p");
+    tokenize("q");
+    tokenize("r");
+    tokenize("s");
+    tokenize("t");
+  }
 
-    #[test]
-    fn tauto() {
-        let actual = tokenize("⊤()");
-    }
+  #[test]
+  fn tauto() {
+    let actual = tokenize("⊤()");
+  }
 
-    #[test]
-    fn falso() {
-        let actual = tokenize("⊥()");
-    }
+  #[test]
+  fn falso() {
+    let actual = tokenize("⊥()");
+  }
 
-    #[test]
-    fn not() {
-        let actual = tokenize("¬(p)");
-    }
+  #[test]
+  fn not() {
+    let actual = tokenize("¬(p)");
+  }
 
-    #[test]
-    fn and() {
-        let actual = tokenize("∧(p,q)");
-    }
+  #[test]
+  fn and() {
+    let actual = tokenize("∧(p,q)");
+  }
 
-    #[test]
-    fn or() {
-        let actual = tokenize("∨(p,q)");
-    }
+  #[test]
+  fn or() {
+    let actual = tokenize("∨(p,q)");
+  }
 
-    #[test]
-    fn imp() {
-        let actual = tokenize("→(p,q)");
-    }
+  #[test]
+  fn imp() {
+    let actual = tokenize("→(p,q)");
+  }
 
-    #[test]
-    fn eq() {
-        let actual = tokenize("↔(p,q)");
-    }
+  #[test]
+  fn eq() {
+    let actual = tokenize("↔(p,q)");
+  }
 }
